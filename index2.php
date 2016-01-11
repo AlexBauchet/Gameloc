0f1b3743479391ffa846beca050affda8ce00f8e
AlexBauchet <alexandrebauchet1@gmail.com>
2016-01-11 11:34:00 +0100

    Revert "ajout"
    This reverts commit accdc2f7705ff0760ee659ea2628ae4c3fbffcf9.

---
+++ index2.php
@@ -0,0 +1,36 @@
+<?php
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+	<title>Accueil</title>
+</head>
+<body>
+
+	<div class="container">
+		<div class="row">
+		  <div class="col-md-5 col-md-offset-3">
+		  <img src="public/img/steam_logo.png"></img>
+		  	<h1>Gameloc</h1>
+		  	<p>Bienvenue dans la plus grande communauté de gamers sur Paris !</p><br />
+		  <p>Nous mettons les gamers en relation pour qu'il puisse s'échanger les jeux vidéos de n'importe quel platforme<br /> (PC, XBOX, PS4)</p>
+
+		 <button type="submit" name="action" action="register.php" class="btn btn-success">Inscription</button>
+		 <button type="log" name="action" class="btn btn-primary">Connexion</button>
+		  </div>
+		 </div> 		 
+	</div>	
+		    
+		
+		
+
+
+</body>
+</html>
